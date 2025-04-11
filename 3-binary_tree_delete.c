06cacfddadc2561921741317e4ee976d0989c4ed
YuriSoma <yaser.alelayan@outlook.com>
2025-04-11 19:04:39 +0000

    Uploading Task-03 solution

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,5 +8,11 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree)
+	{
+		if (tree->left)
+			binary_tree_delete(tree->left);
+		if (tree->right)
+			binary_tree_delete(tree->right);
 		free(tree);
+	}
 }
