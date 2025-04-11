3263b91a426c8e41cd4f6f010042d908cd491f92
“MDDO0” <maado321@gmail.com>
2025-04-11 11:00:30 +0000

    a

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binery_tree_node - Creates new node without child
+ * @parent: a poienter to the parent of the new node
+ * @value: a value to be assigend to the new node
+ *
+ * Return: the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	return (new_node);
+}
