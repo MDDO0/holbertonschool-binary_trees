191b427a6d0a2b427426194ceb4491dc26f81d8a
YuriSoma <yaser.alelayan@outlook.com>
2025-04-12 14:55:26 +0000

    Uplading Task-18 solution

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a given node.
+ * @node: the given node.
+ *
+ * Return: node pointer of the uncle
+ * - NULL if no uncle or given node is NULL.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t uncle;
+
+	if (!node || !node->parent)
+		return (NULL);
+
+	uncle = binary_tree_sibling(node->parent);
+
+	uncle ? return (uncle) : return (NULL);
+}
